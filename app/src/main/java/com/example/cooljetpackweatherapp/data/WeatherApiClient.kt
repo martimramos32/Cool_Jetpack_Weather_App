bb2e1d6f8df7ccd3c336f558680238b1e1394fc1
marti <martim.ramos2005@gmail.com>
2026-04-21 15:52:52 +0100

    Criação do Projeto "Cool Jetpack Weather App". Este projeto consiste numa reconstrução da Weather App desenvolvida no TP2. No trabalho prático anterior construímos esta aplicação recorrendo a XML views, mas neste novo trabalho prático iremos reconstruir a aplicação utilizando Jetpack Compose em vez de XML Views

---
+++ app/src/main/java/com/example/cooljetpackweatherapp/data/WeatherApiClient.kt
@@ -0,0 +1,38 @@
+package com.example.cooljetpackweatherapp.data
+
+import android.net.http.HttpResponseCache.install
+import dam_A51736.coolweatherapp.WeatherData
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object WeatherApiClient {
+    private val client = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            }) // Ignora campos JSON que não precisamos
+        }
+    }
+
+    suspend fun getWeather(lat: Float, lon: Float): WeatherData? {
+        val reqString = buildString {
+            append("https://api.open-meteo.com/v1/forecast?")
+            append("latitude=${lat}&longitude=${lon}&")
+            append("current_weather=true&")
+            append("hourly=temperature_2m,weathercode,pressure_msl,windspeed_10m")
+        }
+        System.out.println("Getting URL: $reqString")
+        return try {
+            client.get(reqString).body() // Transforma o JSON em WeatherData
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cooljetpackweatherapp/data/WeatherData.kt
@@ -0,0 +1,91 @@
+package dam_A51736.coolweatherapp
+
+import kotlinx.serialization.Serializable
+
+//Classe responsável por receber os dados principais da API
+@Serializable
+data class WeatherData(
+    var latitude: String,
+    var longitude: String,
+    var timezone: String,
+    var current_weather: CurrentWeather,
+    var hourly: Hourly,
+    var daily: Daily //Novo campo que armazena os dados da classe Daily
+)
+
+//Nova classe que é responsável por receber os dados de nascer e pôr do sol que serão recebidos da API
+@Serializable
+data class Daily(
+    var sunrise: ArrayList<String>,
+    var sunset: ArrayList<String>
+)
+
+//Classe que guarda os detalhes específicos do tempo atual recebido pela classe WeatherData
+@Serializable
+data class CurrentWeather(
+    var windspeed: Float,
+    var temperature: Float,
+    var winddirection: Int,
+    var weathercode: Int,
+    var time: String
+)
+
+//Classe que permite armazenar a previsão do tempo hora a hora para o resto do dia
+@Serializable
+data class Hourly(
+    var time: ArrayList<String>,
+    var temperature_2m: ArrayList<Float>,
+    var weathercode: ArrayList<Int>,
+    var pressure_msl: ArrayList<Double>
+)
+
+//Lista que serve como um dicionário que liga o "code" ao nome exato da imagem que está guardada na diretoria drawable, para que mais tarde o ecrã saiba logo qual é a imagem correta a apresentar no display
+enum class WMO_WeatherCode(var code: Int, var image: String) {
+    CLEAR_SKY(0, "clear_day"),
+    MAINLY_CLEAR(1, "mostly_clear_day"),
+    PARTLY_CLOUDY(2, "partly_cloudy_day"),
+    OVERCAST(3, "cloudy"),
+    FOG(45, "fog"),
+    DEPOSITING_RIME_FOG(48, "fog"),
+    DRIZZLE_LIGHT(51, "drizzle"),
+    DRIZZLE_MODERATE(53, "drizzle"),
+    DRIZZLE_DENSE(55, "drizzle"),
+    FREEZING_DRIZZLE_LIGHT(56, "freezing_drizzle"),
+    FREEZING_DRIZZLE_DENSE(57, "freezing_drizzle"),
+    RAIN_SLIGHT(61, "rain_light"),
+    RAIN_MODERATE(63, "rain"),
+    RAIN_HEAVY(65, "rain_heavy"),
+    FREEZING_RAIN_LIGHT(66, "freezing_rain_light"),
+    FREEZING_RAIN_HEAVY(67, "freezing_rain_heavy"),
+    SNOW_FALL_SLIGHT(71, "snow_light"),
+    SNOW_FALL_MODERATE(73, "snow"),
+    SNOW_FALL_HEAVY(75, "snow_heavy"),
+    SNOW_GRAINS(77, "snow"),
+    RAIN_SHOWERS_SLIGHT(80, "rain_light"),
+    RAIN_SHOWERS_MODERATE(81, "rain"),
+    RAIN_SHOWERS_VIOLENT(82, "rain_heavy"),
+    SNOW_SHOWERS_SLIGHT(85, "snow_light"),
+    SNOW_SHOWERS_HEAVY(86, "snow_heavy"),
+    THUNDERSTORM_SLIGHT_MODERATE(95, "tstorm"),
+    THUNDERSTORM_HAIL_SLIGHT(96, "tstorm"),
+    THUNDERSTORM_HAIL_HEAVY(99, "tstorm")
+}
+
+//Uma vez que, já existe o enumerado com as informações que receberemos da API é necessário que a aplicação saiba qual a imagem a utilizar para um determinado código, portanto precisamoas de mapear estes valores
+fun getWeatherCodeMap(): Map<Int, WMO_WeatherCode> {
+    val weatherMap = HashMap<Int, WMO_WeatherCode>() //Mapa que para uma determinada chave(code) retornará um determinado valor(imagem correspondente)
+    //lê todas as linhas do enumerado
+    WMO_WeatherCode.values().forEach {
+        weatherMap.put(it.code, it) //Se procurar por um determinado código, retorna essa mesmo linha completa do enumerado
+    }
+    return weatherMap //retorna um mapa completo com todas as correspondencias necessárias para todos os codigos recebidos da API
+}
+
+//ESTRUTURA PARA TRATAR DOS DADOS DO XML RESOURCE
+
+// Classe que representa os dados lidos do XML (contido na pasta res/value)
+data class WeatherCondition(
+    val code: Int,
+    val image: String,
+    val description: String
+)
