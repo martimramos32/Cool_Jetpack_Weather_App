26eae630f3e3155fa62e07a0470d0d5b91cb02b2
marti <martim.ramos2005@gmail.com>
2026-04-24 12:25:43 +0100

    Consolidação do estudo da aplicação em desenvolvimento (Cool Jetpack Weather App). Package ui e viewmodel em desenvolvimento...

---
+++ app/src/main/java/com/example/cooljetpackweatherapp/ui/WeatherUIState.kt
@@ -0,0 +1,13 @@
+package com.example.cooljetpackweatherapp.ui
+
+data class WeatherUIState(
+    val latitude: Float = 38.7223f, // Coordenadas iniciais (ex: Lisboa)
+    val longitude: Float = -9.1393f,
+    val temperature: Float = 0.0f,
+    val seaLevelPressure: Float = 0.0f,
+    val winddirection: Int = 0,
+    val windspeed: Float = 0.0f,
+    val weathercode: Int = 0,
+    val time: String = "--:--",
+    val isDay: Boolean = true // Variável extra para usar com a lógica do nascer/pôr do sol
+)
